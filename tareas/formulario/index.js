2cc7e11f17bf725c38a66ff84df6fa2e2f16442a
Fabricio Leonardo Sodano Pascazi <fabricio.sodano@gmail.com>
2025-04-28 09:58:21 +1000

    Agrega tarea formulario-familia

--- tareas/formulario/index.js
+++ tareas/formulario/index.js
@@ -15,40 +15,54 @@ document.querySelector('form').addEventListener('submit', function(event){
 
     const montoSolicitado = Number(document.querySelector('#monto').value);
 
+    let puedeTransferir = true;
+
     if(cuentaDesde.id === cuentaHacia.id){
-        mostrarError('No se puede transferir entre mismas cuentas');
-        return false;
+        puedeTransferir = false;
+        const errorMismaCuenta = 'No se puede transferir entre mismas cuentas';
+        mostrarError(document.querySelector('#cuenta-desde-error'), errorMismaCuenta);
+        mostrarError(document.querySelector('#cuenta-hacia-error'), errorMismaCuenta);
     }
 
     if(montoSolicitado <= 0){
-        mostrarError('Ingrese un monto mayor a 0');
-        return false;
+        puedeTransferir = false;
+        mostrarError(document.querySelector('#monto-error'),'Ingrese un monto mayor a 0');
     }
 
     const descripcion = document.querySelector('#descripcion').value;
     const regexDescripcion = /^[a-z0-9 ]{3,}$/i;
     if(!regexDescripcion.test(descripcion.trim())){
-        mostrarError('La descripción debe contener sólo letras y números y al menos 3 caracteres');
-        return false;
+        puedeTransferir = false;
+        mostrarError(document.querySelector('#descripcion-error'),'La descripción debe contener sólo letras y números y al menos 3 caracteres');
     }
 
-    if(cuentaDesde.saldo >= montoSolicitado){
-        alert('Transferencia realizada!');
-    }else{
-        mostrarError('la transferencia NO se puede hacer, saldo insuficiente');
-        return false;
+    if(montoSolicitado <= 0){
+        puedeTransferir = false;
+        mostrarError(document.querySelector('#errores'), 'La transferencia NO se puede hacer. El monto debe ser mayor a 0');
     }
+
+    if(cuentaDesde.saldo < montoSolicitado){
+        puedeTransferir = false;
+        mostrarError(document.querySelector('#errores'), 'La transferencia NO se puede hacer. Saldo insuficiente o el monto debe ser mayor a 0');
+    }
+
+    if(puedeTransferir){
+        alert('Transferencia realizada con éxito');
+    }
+
+    return puedeTransferir;
 });
 
 function limpiarErrores(){
-    const $errores = document.querySelector('#errores');
-    $errores.innerHTML = '';
+    const $errores = document.querySelectorAll('.error');
+    for(let i=0; i<$errores.length; i++){
+        $errores[i].textContent = '';
+    }
 }
 
 
-function mostrarError(error){
-    const $errores = document.querySelector('#errores');
-    $errores.innerHTML = `<p>${error}</p>`;
+function mostrarError($elementoError, error){
+    $elementoError.textContent = error;
 }
 
 function mostrarCuentas(){
